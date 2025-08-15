57cd96698a8e3139443e066cf78c2847efffa7cb
facundocarballo <carballofacundo70@gmail.com>
2025-08-15 16:18:21 -0300

    feat: create-deployment-config-script

---
+++ scripts/create-deployment-config.sh
@@ -0,0 +1,74 @@
+#!/bin/bash
+
+FOLDER_PATH=deployments/$1
+FILE_PATH=$FOLDER_PATH/kustomization.yaml
+
+if [ ! -f $FILE_PATH ]; then
+    mkdir -p $FOLDER_PATH
+    touch $FILE_PATH
+fi
+
+cat <<EOF > "$FILE_PATH"
+apiVersion: kustomize.config.k8s.io/v1beta1
+kind: Kustomization
+namespace: microservices
+resources:
+- ../../base
+configMapGenerator:
+- envs:
+  - variables.properties
+  name: $1
+  options:
+    disableNameSuffixHash: true
+patches:
+  # Patch Deployment
+  - target:
+      kind: Deployment
+      name: ms
+    patch: |-
+      - op: replace
+        path: /metadata/name
+        value: $1
+      - op: replace
+        path: /spec/selector/matchLabels/app
+        value: $1
+      - op: replace
+        path: /spec/template/metadata/labels/app
+        value: $1
+      - op: replace
+        path: /spec/template/spec/containers/0/name
+        value: $1
+      - op: replace
+        path: /spec/template/spec/containers/0/image
+        value: $1:latest
+  
+  # Patch Service
+  - target:
+      kind: Service
+      name: ms
+    patch: |-
+      - op: replace
+        path: /metadata/name
+        value: $1
+      - op: replace
+        path: /spec/selector/app
+        value: $1
+
+  # Patch HPA
+  - target:
+      kind: HorizontalPodAutoscaler
+      name: ms
+    patch: |-
+      - op: replace
+        path: /metadata/name
+        value: $1
+      - op: replace
+        path: /spec/scaleTargetRef/name
+        value: $1
+      - op: replace
+        path: /spec/minReplicas
+        value: 1
+      - op: replace
+        path: /spec/maxReplicas
+        value: 3
+EOF
